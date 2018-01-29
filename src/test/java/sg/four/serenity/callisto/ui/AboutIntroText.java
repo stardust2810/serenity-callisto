3f5c9e9fb2b701e7964d0f02218293fe78a00edb
stardust2810 <stardust2810@outlook.com>
2018-01-30 05:50:02 +0800

    using screenplay

---
+++ src/test/java/sg/four/serenity/callisto/ui/AboutIntroText.java
@@ -0,0 +1,7 @@
+package sg.four.serenity.callisto.ui;
+
+import net.serenitybdd.screenplay.targets.Target;
+
+public class AboutIntroText {
+    public static Target INTRO_TEXT = Target.the("intro text").locatedBy(".rte");
+}
