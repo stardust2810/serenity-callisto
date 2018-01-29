e49e473a85519199feeec70fb91e055846eec285
stardust2810 <stardust2810@outlook.com>
2018-01-30 05:50:56 +0800

    using screenplay

--- run.bat
+++ run.bat
@@ -4,5 +4,5 @@ set /p answer=Enter selection:
 if /i "%answer:~,1%" EQU "R" mvn clean verify
 if /i "%answer:~,1%" EQU "C" mvn clean
 if /i "%answer:~,1%" EQU "V" mvn verify
-if /i "%answer:~,1%" EQU "F" mvn clean verify -Dcucumber.options="--tags @workplace"
+if /i "%answer:~,1%" EQU "F" mvn clean verify -Dcucumber.options="--tags @faq"
 EXIT
