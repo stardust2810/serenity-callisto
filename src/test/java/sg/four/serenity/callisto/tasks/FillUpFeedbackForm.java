3d8b2a1bbe37493e6bbfdfe3be9497ff120a8d4b
stardust2810 <stardust2810@outlook.com>
2018-02-01 06:38:43 +0800

    Home 20180201
    Testing Form

--- src/test/java/sg/four/serenity/callisto/tasks/FillUpFeedbackForm.java
+++ src/test/java/sg/four/serenity/callisto/tasks/FillUpFeedbackForm.java
@@ -2,10 +2,13 @@ package sg.four.serenity.callisto.tasks;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.actions.Enter;
+import net.serenitybdd.screenplay.actions.EnterValueIntoTarget;
 import net.serenitybdd.screenplay.actions.Click;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.Keys;
 import sg.four.serenity.callisto.ui.FeedbackForm;
+import org.openqa.selenium.support.ui.Select;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
@@ -14,7 +17,7 @@ public class FillUpFeedbackForm implements Task {
     private final String details;
     private final String email;
     private final String name;
-
+    private static Logger log = LogManager.getLogger(FillUpFeedbackForm.class);
 
     protected FillUpFeedbackForm(String category, String details, String email, String name) {
         this.category = category;
@@ -24,11 +27,35 @@ public class FillUpFeedbackForm implements Task {
     }
 
     public <T extends Actor> void performAs(T actor) {
-        actor.attemptsTo(Enter.theValue(category).into(FeedbackForm.CATEGORY_DROPDOWN).thenHit(Keys.ENTER));
-        actor.attemptsTo(Enter.theValue(details).into(FeedbackForm.DETAILS_TEXTAREA).thenHit(Keys.TAB));
-        actor.attemptsTo(Enter.theValue(email).into(FeedbackForm.EMAIL_TEXTFIELD).thenHit(Keys.TAB));
-        actor.attemptsTo(Enter.theValue(name).into(FeedbackForm.NAME_TEXTFIELD).thenHit(Keys.TAB));
-        actor.attemptsTo(Click.on(FeedbackForm.SUBMIT_BUTTON));
+
+               /*
+        //for debugging purposes
+
+        List<String> a = Text.of(NewsPage.CARD_ITEMS).viewedBy(actor).asList();
+
+        if (a == null) log.error("a is null");
+        else log.info("a is not null");
+
+        if(a.isEmpty())log.error("a is empty");
+        else log.info("Size is " + a.size());
+        */
+               if(FeedbackForm.CATEGORY_DROPDOWN==null) log.error("Form is null");
+               else {
+                   log.info("Dropdown is found");
+                   log.info(FeedbackForm.CATEGORY_DROPDOWN.getCssOrXPathSelector());
+               }
+               try {
+Select a = new Select(FeedbackForm.CATEGORY_DROPDOWN.resolveFor(actor));
+a.selectByValue("Suggestions");
+                  // actor.attemptsTo(Enter.theValue(category).into(FeedbackForm.CATEGORY_DROPDOWN).thenHit(Keys.TAB));
+                   //actor.attemptsTo(Enter.theValue(details).into(FeedbackForm.DETAILS_TEXTAREA).thenHit(Keys.TAB));
+                   //actor.attemptsTo(Enter.theValue(email).into(FeedbackForm.EMAIL_TEXTFIELD).thenHit(Keys.TAB));
+                   actor.attemptsTo(new EnterValueIntoTarget("testhotmail.com", FeedbackForm.EMAIL_TEXTFIELD));
+                   //actor.attemptsTo(Enter.theValue(name).into(FeedbackForm.NAME_TEXTFIELD).thenHit(Keys.TAB));
+                   //actor.attemptsTo(Click.on(FeedbackForm.SUBMIT_BUTTON));
+               }catch (Exception e) {
+                   log.error(e);
+               }
     }
 
     public static FillUpFeedbackForm entersDetails(String category, String details, String email, String name){
